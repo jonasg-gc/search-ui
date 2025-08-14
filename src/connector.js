afb6e07fb3eabbca3de939324a3d7c2e1b8245f9
Jonas <jonas.graham@hrsdc-rhdcc.gc.ca>
2025-08-14 08:42:55 -0400

    Update connector.js

--- src/connector.js
+++ src/connector.js
@@ -39,7 +39,7 @@ const defaults = {
 	"isContextSearch": false,
 	"isAdvancedSearch": false,
 	"originLevel3": window.location.origin + winPath,
-	"Pipeline": "",
+	"pipeline": "",
 	"minimumSnippetScore": 0.7
 };
 let lang = document.querySelector( "html" )?.lang;
@@ -521,7 +521,7 @@ function initEngine() {
 			search: {
 				locale: params.lang,
 				searchHub: params.searchHub,
-				Pipeline: params.Pipeline,
+				pipeline: params.pipeline,
 				preprocessSearchResponseMiddleware: (response) => {
 					searchResult = response.body;
 					return response;
